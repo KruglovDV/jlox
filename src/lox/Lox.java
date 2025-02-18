0e8e9fafb6c2aecd95b5b304ac6ed2bce89ece78
DmitryK <kruglov081289@gmail.com>
2025-02-18 21:18:47 +0300

    add interpreter

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         // var args = new String[] {"./fixtures/simple-script"};
@@ -28,6 +30,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     public static void runPrompt() throws IOException {
@@ -49,12 +52,9 @@ public class Lox {
        Parser parser = new Parser(tokens);
        Expr expr = parser.parse();
 
-       if (hadError) {
-           System.out.println("had error");
-           return;
-       }
+       if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expr));
+       interpreter.interpret(expr);
     }
 
     static void error(int line, String message) {
@@ -67,6 +67,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     static void report(int line, String where, String message) {
